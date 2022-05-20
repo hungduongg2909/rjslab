dee05bc95670d940a549051c5e84e8296bc5ed41
HungDuong <hungduongg2909@gmail.com>
2022-05-20 23:29:39 +0700

    LAB03_2 done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 // import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import React, { Component } from 'react';
 import Menu from './Components/Menu';
 import './App.css';
+import { DISHES } from './share/dishes';
 
-function App() {
-  return (
-    <div>
-        <Navbar dark color="primary">
-            <div className="container">
-                <NavbarBrand href="/">Ristorante Confusion</NavbarBrand>
-            </div>
-        </Navbar>
+class App extends Component {
 
-        <Menu />
-    </div>
-  );
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+          dishes: DISHES
+      };
+    }
+  
+    render() {
+
+        return (
+          <div>
+              <Navbar dark color="primary">
+                  <div className="container">
+                      <NavbarBrand href="/">Ristorante Confusion</NavbarBrand>
+                  </div>
+              </Navbar>
+      
+              <Menu dishes={this.state.dishes} />
+          </div>
+        );
+    
+    }
 }
 
 export default App;
