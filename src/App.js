845a3eafc8c9fae61cf1cf20ea4469b74e69cbb7
HungDuong <hungduongg2909@gmail.com>
2022-05-29 23:11:00 +0700

    init lab04

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-// import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import React, { Component } from 'react';
-import Menu from './Components/Menu';
 import './App.css';
-import { DISHES } from './share/dishes';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
-
     constructor(props) {
       super(props);
-  
       this.state = {
-          dishes: DISHES
+        dishes: DISHES
       };
     }
-  
-    render() {
 
-        return (
-          <div>
-              <Navbar dark color="primary">
-                  <div className="container">
-                      <NavbarBrand href="/">Ristorante Confusion</NavbarBrand>
-                  </div>
-              </Navbar>
-      
-              <Menu dishes={this.state.dishes} />
-          </div>
-        );
-    
-    }
+  render() {
+    return (
+      <div className="App">
+         <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem, Label,
+    Modal, ModalHeader, ModalBody, Button, Row, Col } from 'reactstrap';
+
+
+class DishDetail extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        }
+    }
+
+
+    render() {
+        if (this.props.dish != null)
+            return(
+                <div className="row">
+                    <div  className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+                            <CardBody>
+                            <CardTitle>{this.props.dish.name}</CardTitle>
+                            <CardText>{this.props.dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+
+                    <div className="col-12 col-md-5 m-1">
+                        <h4>Comments</h4>
+                        <ul className="list-unstyled">
+                            {
+                                this.props.dish.comments.map((comment) => {
+                                    return (
+                                        <div in key={comment._id}>
+                                            <li>
+                                            <p>{comment.comment}</p>
+                                            <p>{comment.rating} stars</p>
+                                            <p>
+                                                -- {comment.author} ,
+                                                {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'})
+                                                .format(new Date(Date.parse(comment.date)))}
+                                            </p>
+                                            </li>
+                                        </div>
+                                    );
+                                })
+                            }
+                        </ul>
+                    </div>
+                </div>
+
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+}
+
+export default DishDetail;
