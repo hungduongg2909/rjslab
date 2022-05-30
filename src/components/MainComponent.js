9cb4399d74c1401a8b7ee54a9841571f5dededbf
HungDuong <hungduongg2909@gmail.com>
2022-05-30 16:52:14 +0700

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,36 +1,45 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { Routes, Route, Navigate } from 'react-router-dom';
+/* import { Switch, Route, Redirect } from 'react-router-dom';
+        Switch & Redirect has been removed from React router v6, and replaced Routes and Navigate*/
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        dishes: DISHES,
-        selectedDish: null
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
+    
+    render() {
+        const _dishes = this.state.dishes;
 
-  render() {
-    return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} onClick={ dishId => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter( dish => dish.id === this.state.selectedDish)[0]} />
-      </div>
-    );
-  }
+        return (
+            <div>
+                <Header />
+
+                <Routes>
+                    <Route path='/home' element={<Home />} />
+                    <Route exact path='/menu' element={<Menu dishes={_dishes} />} />
+
+                    <Route path="/" element={<Navigate to="/home" replace />} />
+                    {/* <Route path='/home' component={HomePage} />
+                    'component' replaced 'element', must set value is "Component" */}
+                </Routes>
+
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default Main;
